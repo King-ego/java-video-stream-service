1f09dae62b0a31a68ba084d85f2144f0df4c9575
Diego <diegosantos4957@gmail.com>
2026-03-23 15:44:16 -0300

    feat: implement VideoController for video API endpoints

---
+++ src/main/java/com/streem/video/controller/VideoController.java
@@ -0,0 +1,10 @@
+package com.streem.video.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/videos")
+class VideoController {
+
+}
