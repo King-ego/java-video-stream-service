b23147be08d3b5f7c16d6f3f22bc6b65567e3943
Diego <diegosantos4957@gmail.com>
2026-03-24 11:07:04 -0300

    feat: add uploadVideo endpoint to VideoController for video uploads

--- src/main/java/com/streem/video/controller/VideoController.java
+++ src/main/java/com/streem/video/controller/VideoController.java
@@ -2,6 +2,7 @@ package com.streem.video.controller;
 
 import com.streem.video.services.VideoService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
@@ -20,6 +21,12 @@ class VideoController {
     public String getVideos(MultipartFile file) {
         this.videoService.processVideo();
         return "List of videos";
+    }
+
+    @PostMapping
+    public String uploadVideo(MultipartFile file) {
+        this.videoService.processVideo();
+        return "Upload of videos";
     }
 
 }
