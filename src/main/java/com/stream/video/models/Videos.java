ce5b1bbe5e6471324d236e0feec223ee3f78fabd
Diego <diegosantos4957@gmail.com>
2026-03-24 16:29:30 -0300

    feat: simplify Videos entity by using Lombok for getters and setters

--- src/main/java/com/stream/video/models/Videos.java
+++ src/main/java/com/stream/video/models/Videos.java
@@ -1,12 +1,14 @@
 package com.stream.video.models;
 
 import jakarta.persistence.*;
+import lombok.Data;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Entity
 @Table(name = "videos")
+@Data
 public class Videos {
     @Id
     @GeneratedValue
@@ -29,43 +31,4 @@ public class Videos {
         this.updatedAt = LocalDateTime.now();
     }
 
-    public UUID getId() {
-        return id;
-    }
-
-    public void setId(UUID id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public LocalDateTime getCreatedAt() {
-        return createdAt;
-    }
-
-    public void setCreatedAt(LocalDateTime createdAt) {
-        this.createdAt = createdAt;
-    }
-
-    public LocalDateTime getUpdatedAt() {
-        return updatedAt;
-    }
-
-    public void setUpdatedAt(LocalDateTime updatedAt) {
-        this.updatedAt = updatedAt;
-    }
 }
