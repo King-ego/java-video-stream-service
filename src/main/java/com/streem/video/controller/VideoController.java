4d8c157d46c5af0649d86be221aa1caa323f1e30
Diego <diegosantos4957@gmail.com>
2026-03-24 11:00:15 -0300

    feat: add getVideos endpoint to VideoController for retrieving video list

--- src/main/java/com/streem/video/controller/VideoController.java
+++ src/main/java/com/streem/video/controller/VideoController.java
@@ -1,10 +1,17 @@
 package com.streem.video.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequestMapping("/api/videos")
 class VideoController {
+
+    @GetMapping
+    public String getVideos(MultipartFile file) {
+        return "List of videos";
+    }
 
 }
