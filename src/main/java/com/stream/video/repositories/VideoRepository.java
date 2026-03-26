780cf2022790f2d6e0c28ca6771d729ac741fb3b
Diego <diegosantos4957@gmail.com>
2026-03-25 22:58:58 -0300

    feat: implement VideoRepository interface for video data management

--- src/main/java/com/stream/video/repositories/VideoRepository.java
+++ src/main/java/com/stream/video/repositories/VideoRepository.java
@@ -1,4 +1,10 @@
 package com.stream.video.repositories;
 
-public class VideoRepository {
-}
+import com.stream.video.models.Videos;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface VideoRepository extends JpaRepository<Videos, UUID>{}
