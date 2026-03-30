56ce7ae585821fc123aac5a38cb699ff54242734
Diego <diegosantos4957@gmail.com>
2026-03-30 16:59:10 -0300

    fix: add @NonNull annotation to generics in VideoRepository for improved null safety

--- src/main/java/com/stream/video/repositories/VideoRepository.java
+++ src/main/java/com/stream/video/repositories/VideoRepository.java
@@ -1,11 +1,12 @@
 package com.stream.video.repositories;
 
 import com.stream.video.models.Videos;
+import lombok.NonNull;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
 @Repository
-public interface VideoRepository extends JpaRepository<Videos, UUID>{};
+public interface VideoRepository extends JpaRepository<@NonNull Videos, @NonNull UUID>{};
 
