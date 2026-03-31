1a65588b8777bc03e11ce9012ebe163f83bff97e
Diego <diegosantos4957@gmail.com>
2026-03-31 17:00:17 -0300

    feat: add updateParseFields method to VideoRepository for updating video filename

--- src/main/java/com/stream/video/repositories/VideoRepository.java
+++ src/main/java/com/stream/video/repositories/VideoRepository.java
@@ -3,10 +3,19 @@ package com.stream.video.repositories;
 import com.stream.video.models.Videos;
 import lombok.NonNull;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
 @Repository
-public interface VideoRepository extends JpaRepository<@NonNull Videos, @NonNull UUID>{}
+public interface VideoRepository extends JpaRepository<@NonNull Videos, @NonNull UUID>{
+    @Modifying
+    @Query("UPDATE Videos v SET v.filename = COALESCE(:fieldName, v.filename) WHERE v.id = :video_id")
+    void updateParseFields(
+            String video_id,
+            String fieldName
+    );
+}
 
