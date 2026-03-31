128e4a75314c3457786ff277b64b04a1e1fd6a4d
Diego <diegosantos4957@gmail.com>
2026-03-31 16:50:01 -0300

    fix: remove unnecessary semicolon at end of VideoRepository.java for cleaner code

--- src/main/java/com/stream/video/repositories/VideoRepository.java
+++ src/main/java/com/stream/video/repositories/VideoRepository.java
@@ -8,5 +8,5 @@ import org.springframework.stereotype.Repository;
 import java.util.UUID;
 
 @Repository
-public interface VideoRepository extends JpaRepository<@NonNull Videos, @NonNull UUID>{};
+public interface VideoRepository extends JpaRepository<@NonNull Videos, @NonNull UUID>{}
 
